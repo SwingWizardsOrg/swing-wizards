44ebc7b18340a80d97c2887c29d23c7d9ed75da6
gregorymark254 <gregorymark254@gmail.com>
2023-04-29 12:43:44 +0300

    Swing Wizards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Nav from './Pages/Nav'
+import Home from './Pages/Home'
+import Footer from './Pages/Footer'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+      <Footer/>
     </div>
   );
 }
