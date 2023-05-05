66b8e654aceb1dbc17487319918a3732f265c797
gregorymark254 <gregorymark254@gmail.com>
2023-05-05 13:26:07 +0300

    Updated home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,61 @@
 import React from 'react'
+import about1 from '../images/about1.png'
+import about2 from '../images/about2.png'
+import about3 from '../images/about3.png'
 
 const Home = () => {
   return (
     <div>
-      Home
+      <section className="hero">
+        <div className="container mx-auto p-4">
+          <div className="flex flex-wrap justify-center gap-10 mb-5">
+            <h3>&#10004; Free Investment</h3>
+            <h3>&#10004; Free Education</h3>
+            <h3>&#10004; A Great Community</h3>
+          </div>
+          <div className="flex flex-wrap justify-center gap-10">
+            <button className="px-5 py-2 bg-blue-950 text-white rounded-xl">LOGIN</button>
+            <button className="px-5 py-3 bg-red-700 text-white rounded-xl">SIGN IN</button>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="container mx-auto p-4 text-white my-20">
+          <div className="flex flex-wrap justify-evenly text-center items-center gap-5">
+            <div className="border-8 border-blue-950 p-4 rounded-3xl">
+              <div>
+                <img width={300} height={500} src={about1} alt="about" />
+              </div>
+              <br />
+              <div className="space-y-3">
+                <h3>INVESTMENT APP</h3>
+                <button className="px-5 py-2 bg-blue-950  rounded-lg"><a href="/#">GO</a></button>
+              </div>
+            </div>
+            <div className="border-8 border-blue-950 p-4 rounded-3xl">
+              <div>
+                <img width={300} height={500} src={about2} alt="about" />
+              </div>
+              <br />
+              <div className="space-y-3">
+                <h3>TRADING</h3>
+                <button className="px-5 py-2 bg-blue-950 rounded-lg"><a href="/#">GO</a></button>
+              </div>
+            </div>
+            <div className="border-8 border-blue-950 p-4 rounded-3xl">
+              <div>
+                <img width={300} height={300} src={about3} alt="about" />
+              </div>
+              <br />
+              <div className="space-y-3">
+                <h3>COMMUNITY</h3>
+                <button className="px-5 py-2 bg-blue-950 rounded-lg"><a href="/#">GO</a></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
--- src/Pages/Nav.js
+++ src/Pages/Nav.js
@@ -1,9 +1,80 @@
-import React from 'react'
+import React,{ useState } from 'react'
+
 
 const Nav = () => {
+
+  const [navIsShown, setnavIsShown] = useState(false);
+  const toggleNavIsShown = () => {
+    setnavIsShown((navIsShown) => !navIsShown);
+  };
+
+
   return (
-    <div>
-      Nav
+    <div className="nav">
+      <div className="text-white shadow shadow-neutral-700">
+        <div className="container mx-auto py-4">
+          <nav className='flex justify-between items-center px-4'>
+            {/* Top Nav */}
+            <div className="p-2">
+              <h2><b><span className="text-blue-900">S</span>wing <span className="text-blue-900">W</span>izard</b></h2>
+            </div>
+            <div>
+              <ul className='hidden md:flex text-xl p-2'>
+                <li>
+                  <a href="/" className="hover:text-[#4F5DEC] px-3 py-2 ">Investment</a>
+                </li>
+                <li>
+                  <a href="/cars" className="hover:text-[#4F5DEC] px-3 py-2 ">Web School</a>
+                </li>
+                <li>
+                  <a href="/about" className="hover:text-[#4F5DEC] px-3 py-2 ">Community</a>
+                </li>
+                <li>
+                  <a href="/services" className="hover:text-[#4F5DEC] px-3 py-2 ">About Us</a>
+                </li>
+                <li>
+                  <a href="/contact" className="hover:text-[#4F5DEC] px-3 py-2">Create Account</a>
+                </li>
+              </ul>
+            </div>
+
+            
+            {/* Responsive Nav */}
+            {!navIsShown ? (
+              <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-6 h-6 md:hidden' onClick={toggleNavIsShown} >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25' />
+              </svg>
+              
+            ) : (          
+              <svg
+                xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth={1.5} stroke='currentColor' className='w-7 h-7 z-50' onClick={toggleNavIsShown} >
+                <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25' />
+              </svg>
+            )}
+            {navIsShown && (
+              <div className='md:hidden absolute z-10 top-12 left-0  w-full text-black'>
+                <ul className='text-center mb-2 bg-white p-2 mx-4'>
+                  <li>
+                    <a href="/" className="hover:text-[#4F5DEC] px-3 py-2 ">Investment</a>
+                  </li>
+                  <li>
+                    <a href="/cars" className="hover:text-[#4F5DEC] px-3 py-2 ">Web School</a>
+                  </li>
+                  <li>
+                    <a href="/about" className="hover:text-[#4F5DEC] px-3 py-2 ">Community</a>
+                  </li>
+                  <li>
+                    <a href="/services" className="hover:text-[#4F5DEC] px-3 py-2 ">About Us</a>
+                  </li>
+                  <li>
+                    <a href="/contact" className="hover:text-[#4F5DEC] px-3 py-2">Create Account</a>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </nav>
+        </div>
+      </div>
     </div>
   )
 }
