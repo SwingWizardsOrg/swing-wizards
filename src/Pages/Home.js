a62294faed7252e4e7cb223cf0f7c7023737b7c7
gregorymark254 <gregorymark254@gmail.com>
2023-05-07 12:19:32 +0300

    Image resizing

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -14,43 +14,43 @@ const Home = () => {
             <h3>&#10004; A Great Community</h3>
           </div>
           <div className="flex flex-wrap justify-center gap-10">
-            <button className="px-5 py-2 bg-blue-950 text-white rounded-xl">SIGN IN</button>
-            <button className="px-5 py-3 bg-red-700 text-white rounded-xl">SIGN UP</button>
+            <button className="px-5 py-2 bg-blue-950 hover:bg-blue-900 text-white rounded-xl">SIGN IN</button>
+            <button className="px-5 py-3 bg-red-700 hover:bg-red-600 text-white rounded-xl">SIGN UP</button>
           </div>
         </div>
       </section>
 
       <section>
         <div className="container mx-auto p-4 text-white my-20">
-          <div className="flex flex-wrap justify-evenly text-center items-center gap-5">
+          <div className="flex flex-wrap justify-evenly text-center items-center p-4 gap-5">
             <div className="border-8 border-blue-950 p-4 rounded-3xl">
               <div>
-                <img width={300} height={500} src={about1} alt="about" />
+                <img width={300} height={500} className="h-72 object-contain" src={about1} alt="about" />
               </div>
               <br />
               <div className="space-y-3">
                 <h3>INVESTMENT APP</h3>
-                <button className="px-5 py-2 bg-blue-950  rounded-lg"><a href="/#">GO</a></button>
+                <button className="px-5 py-2 bg-blue-950  hover:bg-blue-900 rounded-lg"><a href="/#">GO</a></button>
               </div>
             </div>
             <div className="border-8 border-blue-950 p-4 rounded-3xl">
               <div>
-                <img width={300} height={500} src={about2} alt="about" />
+                <img width={300} height={500} className="h-72 object-contain" src={about2} alt="about" />
               </div>
               <br />
               <div className="space-y-3">
                 <h3>TRADING</h3>
-                <button className="px-5 py-2 bg-blue-950 rounded-lg"><a href="/#">GO</a></button>
+                <button className="px-5 py-2 bg-blue-950 hover:bg-blue-900 rounded-lg"><a href="/#">GO</a></button>
               </div>
             </div>
             <div className="border-8 border-blue-950 p-4 rounded-3xl">
               <div>
-                <img width={300} height={300} src={about3} alt="about" />
+                <img width={300} height={300} src={about3} className="h-72 object-contain" alt="about" />
               </div>
               <br />
               <div className="space-y-3">
                 <h3>COMMUNITY</h3>
-                <button className="px-5 py-2 bg-blue-950 rounded-lg"><a href="/#">GO</a></button>
+                <button className="px-5 py-2 bg-blue-950 hover:bg-blue-900 rounded-lg"><a href="/#">GO</a></button>
               </div>
             </div>
           </div>
