2fbe905e6816aa0642a4b69bf57b015bb72af03b
leonkenyaga <jkenyaga@gmail.com>
2023-06-01 22:02:19 +0300

    signup feature

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+import styles from "./css/signup.module.css"
+const SignUp = () => {
+
+  const [inputs, setInputs] = useState({});
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputs);
+  }
+ 
+  return (
+ <div className={styles.main1}>
+  <div className={styles.subdiv}>
+  
+  </div>
+  <div className={styles.subdiv1}>
+<h1>Create your account</h1>
+  </div>
+<div className={styles.subdiv2}>
+  <form>
+  <div className={styles.input_div}>
+      <input type="text" className={styles.input} placeholder="Name" name="username" onChange={handleChange}/>
+    </div>
+    <div className={styles.input_div}>
+      <input type="text"className={styles.input} placeholder="Email" name="email" onChange={handleChange}/>
+    </div> 
+    <div className={styles.input_div}>
+      <input type="text"className={styles.input} placeholder="Password" name="password" onChange={handleChange}/>
+    </div>
+    <div className={styles.submission_div}>
+    <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign up</button>
+    </div>
+  </form>
+    </div>
+    
+
+  </div>
+
+  );
+};
+  
+export default SignUp;
