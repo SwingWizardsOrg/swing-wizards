d027becaaf1f2e9d2429c2b21381d438d5ea675d
gregorymark254 <gregorymark254@gmail.com>
2023-06-01 16:59:24 +0300

    Page Updates

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      Login
+      <div className="bg-blue-950 border-b-2 border-white">
+        <div className="container mx-auto p-4">
+          <div className="flex items-center justify-center">
+            <h3>Account</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -3,7 +3,13 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      Register
+      <div className="bg-blue-950 border-b-2 border-white">
+        <div className="container mx-auto p-4">
+          <div className="flex items-center justify-center">
+            <h3>Register</h3>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
