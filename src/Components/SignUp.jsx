7d08d7b88e9c55565b2faa26d3325905ad2471c7
gregorymark254 <gregorymark254@gmail.com>
2023-06-02 10:23:20 +0300

    Login and register update

--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -14,33 +14,46 @@ const SignUp = () => {
   }
  
   return (
- <div className={styles.main1}>
-  <div className={styles.subdiv}>
-  
-  </div>
-  <div className={styles.subdiv1}>
-<h1>Create your account</h1>
-  </div>
-<div className={styles.subdiv2}>
-  <form>
-  <div className={styles.input_div}>
-      <input type="text" className={styles.input} placeholder="Name" name="username" onChange={handleChange}/>
+    <div className={styles.main1}>
+      <div className={styles.subdiv}></div>
+      <div className={styles.subdiv1}>
+        <h1>Create your account</h1>
+      </div>
+      <div className={styles.subdiv2}>
+        <form>
+          <div className={styles.input_div}>
+            <input 
+              type="text" 
+              className={styles.input} 
+              placeholder="Name" 
+              name="username" 
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.input_div}>
+            <input 
+              type="email"
+              className={styles.input} 
+              placeholder="Email" 
+              name="email" 
+              onChange={handleChange}
+            />
+          </div> 
+          <div className={styles.input_div}>
+            <input 
+              type="password"
+              className={styles.input} 
+              placeholder="Password" 
+              name="password" 
+              onChange={handleChange}
+            />
+          </div>
+          <div className={styles.submission_div}>
+            <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign up</button>
+          </div>
+        </form>
+      </div>
     </div>
-    <div className={styles.input_div}>
-      <input type="text"className={styles.input} placeholder="Email" name="email" onChange={handleChange}/>
-    </div> 
-    <div className={styles.input_div}>
-      <input type="text"className={styles.input} placeholder="Password" name="password" onChange={handleChange}/>
-    </div>
-    <div className={styles.submission_div}>
-    <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign up</button>
-    </div>
-  </form>
-    </div>
-    
-
-  </div>
-
   );
 };
   
--- src/Components/Signinbox.js
+++ src/Components/Signinbox.js
@@ -15,27 +15,35 @@ const Signinbox = () => {
   }
  
   return (
-  <div className={styles.main}>
-    <div className={styles.subdiv}>
-    <div className={styles.image_div}>
-    </div>
-    <div className={styles.subdiv2}>
-    <form>
-  <div className={styles.input_div}>
-      <input type="text" className={styles.input} placeholder="Username" name="username" onChange={handleChange}/>
-    </div>
-    <div className={styles.input_div}>
-      <input type="text"className={styles.input} placeholder="Password" name="password" onChange={handleChange}/>
-    </div> 
-    <div className={styles.submission_div}>
-    <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign In</button>
-    </div>
-  </form>
-
-    </div>
-
-    </div>
-      
+    <div className={styles.main}>
+      <div className={styles.subdiv}>
+        <div className={styles.image_div}></div>
+        <div className={styles.subdiv2}>
+          <form>
+            <div className={styles.input_div}>
+              <input 
+                type="text" 
+                className={styles.input} 
+                placeholder="Username" 
+                name="username" 
+                onChange={handleChange}
+              />
+            </div>
+            <div className={styles.input_div}>
+              <input 
+                type="password"
+                className={styles.input} 
+                placeholder="Password" 
+                name="password" 
+                onChange={handleChange}
+              />
+            </div> 
+            <div className={styles.submission_div}>
+              <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign In</button>
+            </div>
+          </form>
+        </div>
+      </div> 
     </div>
   );
 };
