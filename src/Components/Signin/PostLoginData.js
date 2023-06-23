e65a007ab7a1a10ffb36c974d688b9f751bde6ce
gregprymark254 <gregorymark254@gmail.com>
2023-06-23 19:49:13 +0300

    file changes

--- src/Components/Signin/PostLoginData.js
+++ src/Components/Signin/PostLoginData.js
@@ -14,6 +14,7 @@ const PostLogininData = (Data) => {
       console.log(response);
     })
     .catch((error) => {
+      console.log(error.response);
       console.log(error);
     });
 };
--- src/Components/Signin/Signinbox.js
+++ src/Components/Signin/Signinbox.js
@@ -3,26 +3,13 @@ import styles from "../css/Loginpage.module.css";
 import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
-<<<<<<< HEAD:src/Components/Signinbox.js
-import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify';
-import axios from './api'
-
-const URL = '/tradex/user/login'
-
-=======
 import PostLogininData from "./PostLoginData";
->>>>>>> leon:src/Components/Signin/Signinbox.js
-const Signinbox = () => {
 
-  const [username,setUsername] = useState('')
-  const [password,setPassword] = useState('')
+const Signinbox = () => {
+  const [inputs, setInputs] = useState({});
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(eyeOff);
-  const navigate = useNavigate()
 
-<<<<<<< HEAD:src/Components/Signinbox.js
-=======
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -33,7 +20,6 @@ const Signinbox = () => {
     console.log(inputs);
     PostLogininData(inputs)
   };
->>>>>>> leon:src/Components/Signin/Signinbox.js
   const handleToggle = () => {
     if (type === "password") {
       setIcon(eye);
@@ -44,38 +30,19 @@ const Signinbox = () => {
     }
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      await axios.post(URL,
-      JSON.stringify({ username, password }),
-      {
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        withCredentials: true,
-      });
-      toast.success("Login Successful");
-      navigate("/");
-    } catch (error) {
-      console.log(error)
-      toast.error("Wrong username or password")
-    }
-  };
-
-
   return (
     <div className={styles.main}>
       <div className={styles.subdiv}>
         <div className={styles.image_div}></div>
         <div className={styles.subdiv2}>
-          <form onSubmit={handleSubmit}>
+          <form>
             <div className={styles.input_div}>
               <input
                 type="text"
                 className={styles.input1}
                 placeholder="Username"
                 name="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
+                onChange={handleChange}
               />
             </div>
             <div className={styles.input_div}>
@@ -85,17 +52,21 @@ const Signinbox = () => {
                 placeholder="Password"
                 name="password"
                 autoComplete="current-password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handleChange}
               />
               <span className={styles.eyeIcon} onClick={handleToggle}>
                 <Icon icon={icon} size={20} />
               </span>
             </div>
             <div className={styles.submission_div}>
-              <button type="submit" className={styles.submission_button}>Sign In</button>
+              <button
+                type="submit"
+                className={styles.submission_button}
+                onClick={handleSubmit}
+              >
+                Sign In
+              </button>
             </div>
-            <span><a href="/login">Register Here</a></span>
           </form>
         </div>
       </div>
