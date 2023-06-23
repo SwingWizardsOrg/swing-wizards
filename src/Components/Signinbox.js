9ba867b530dac29e13f64baddb698bac0e5abece
gregorymark254 <gregorymark254@gmail.com>
2023-06-23 13:20:38 +0300

    login page configuration

--- src/Components/Signinbox.js
+++ src/Components/Signinbox.js
@@ -3,20 +3,20 @@ import styles from "./css/Loginpage.module.css";
 import { Icon } from "react-icons-kit";
 import { eyeOff } from "react-icons-kit/feather/eyeOff";
 import { eye } from "react-icons-kit/feather/eye";
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify';
+import axios from './api'
+
+const URL = '/tradex/user/login'
+
 const Signinbox = () => {
-  const [inputs, setInputs] = useState({});
+
+  const [username,setUsername] = useState('')
+  const [password,setPassword] = useState('')
   const [type, setType] = useState("password");
   const [icon, setIcon] = useState(eyeOff);
+  const navigate = useNavigate()
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-    setInputs((values) => ({ ...values, [name]: value }));
-  };
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(inputs);
-  };
   const handleToggle = () => {
     if (type === "password") {
       setIcon(eye);
@@ -27,19 +27,38 @@ const Signinbox = () => {
     }
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(URL,
+      JSON.stringify({ username, password }),
+      {
+        headers: { "Content-Type": "application/json" },
+        withCredentials: true,
+      });
+      toast.success("Login Successful");
+      navigate("/");
+    } catch (error) {
+      console.log(error)
+      toast.error("Wrong username or password")
+    }
+  };
+
+
   return (
     <div className={styles.main}>
       <div className={styles.subdiv}>
         <div className={styles.image_div}></div>
         <div className={styles.subdiv2}>
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className={styles.input_div}>
               <input
                 type="text"
                 className={styles.input1}
                 placeholder="Username"
                 name="username"
-                onChange={handleChange}
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
               />
             </div>
             <div className={styles.input_div}>
@@ -49,14 +68,15 @@ const Signinbox = () => {
                 placeholder="Password"
                 name="password"
                 autoComplete="current-password"
-                onChange={handleChange}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <span className={styles.eyeIcon} onClick={handleToggle}>
                 <Icon icon={icon} size={20} />
               </span>
             </div>
             <div className={styles.submission_div}>
-              <button type="submit" className={styles.submission_button} onClick={handleSubmit}>Sign In</button>
+              <button type="submit" className={styles.submission_button}>Sign In</button>
             </div>
             <span><a href="/login">Register Here</a></span>
           </form>
