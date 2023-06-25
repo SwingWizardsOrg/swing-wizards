54303d06af3ec28481b2094e463ba2e1b979ea53
gregorymark254 <gregorymark254@gmail.com>
2023-06-25 23:48:30 +0300

    toast notifications added

--- src/Components/Registration/PostRegistrationData.js
+++ src/Components/Registration/PostRegistrationData.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 const PostRegistrationData = (Data) => {
   const qs = require("qs");
@@ -15,9 +16,20 @@ const PostRegistrationData = (Data) => {
     )
     .then((response) => {
       console.log(response);
+      toast.success('Registration Successful')
     })
     .catch((error) => {
+      if (!error?.response) {
+        toast.error("No Server Response");
+      } else if (error.response?.status === 401) {
+        toast.error("All fields are required");
+      } else if (error.response?.status === 404) {
+        toast.error("Username or Email Taken");
+      } else {
+        toast.error("Registration Failed");
+      }
       console.log(error);
+      console.log(error.response);
     });
 };
 
--- src/Components/Signin/PostLoginData.js
+++ src/Components/Signin/PostLoginData.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from 'react-toastify';
 
 const PostLogininData = (Data) => {
   const qs = require("qs");
@@ -12,8 +13,16 @@ const PostLogininData = (Data) => {
     )
     .then((response) => {
       console.log(response);
+      toast.success('Login Successful')
     })
     .catch((error) => {
+      if (!error?.response) {
+        toast.error("No Server Response");
+      } else if (error.response?.status === 409) {
+        toast.error("Username of password required");
+      }else {
+        toast.error("Login Failed");
+      }
       console.log(error.response);
       console.log(error);
     });
