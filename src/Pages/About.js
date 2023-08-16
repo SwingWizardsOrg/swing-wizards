4523e1ac4555966b80662f278c87d8ae9ba60cbb
leonkenyaga <jkenyaga@gmail.com>
2023-08-17 01:05:16 +0300

    code formatting

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
@@ -11,7 +11,7 @@ const About = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
