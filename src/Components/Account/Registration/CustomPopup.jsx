99b0a7c00632b85e45e28c52b5f35f655d5b6a0d
leonkenyaga <jkenyaga@gmail.com>
2023-08-17 00:59:20 +0300

    pop up component

---
+++ src/Components/Account/Registration/CustomPopup.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import popupStyles from "../../css/custom-popup.module.css";
+import PropTypes from "prop-types";
+const CustomPopup = (props) => {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(props.show);
+  }, [props.show]);
+
+  return (
+    <div
+      style={{
+        visibility: show ? "visible" : "hidden",
+        opacity: show ? "1" : "0",
+      }}
+      className={popupStyles.overlay}
+    >
+      <div className={popupStyles.popup}>
+        <h2>{props.title}</h2>
+        <div className={popupStyles.content}>{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+CustomPopup.propTypes = {
+  title: PropTypes.string.isRequired,
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
+export default CustomPopup;
