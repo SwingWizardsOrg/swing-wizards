4e00f51a527743e4e38bf33a7e3c969f518b9177
leonkenyaga <jkenyaga@gmail.com>
2023-09-01 11:34:46 +0300

    packaged product cards into the product card container component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,21 +1,19 @@
 import React from "react";
-import Swingappbar from "../Components/bars/Swingappbar";
-import ProductCard from "../Components/Cards/Productcard";
+import {Swingappbar} from "../Components/bars/Swingappbar";
+import {ProductCard} from "../Components/Cards/Productcard";
 import styles from  "../Components/css/Home.module.css"
+import { Box } from "@mui/material";
+import { Landingimage } from "../Components/Landingcomponents/Landingimage";
+import { Productcardcontainers } from "../Components/Landingcomponents/productcardcontainers";
 
-const Home = () => {
+const Home = ({switchmode}) => {
   return (
     <div>
-      <Swingappbar />
-      <section className="hero">
-        <ProductCard Productname="Hooli"/>
-        <ProductCard Productname="TradeShare"/>
-        <ProductCard Productname="SwingTrader"/>
-      </section>
-<div className={styles.aboutdiv}>
-</div>
+      <Swingappbar switchmode={switchmode}/>
+      <Landingimage />   
+      <Productcardcontainers />
       </div>
   );
 };
 
-export default Home;
+export {Home};
