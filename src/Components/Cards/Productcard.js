17a8abd251053fe917d5eaca55bb95d814b20b06
leonkenyaga <jkenyaga@gmail.com>
2023-09-01 11:28:24 +0300

    revised productcard styling

--- src/Components/Cards/Productcard.js
+++ src/Components/Cards/Productcard.js
@@ -4,16 +4,15 @@ import CardContent from '@mui/material/CardContent';
 import { CardActionArea } from '@mui/material';
 import styles from "../css/Card.module.css";
 
-
 const ProductCard =({Productname})=>{
   return (
-    <Card sx={{mt:60, ml:3}}>
-      <CardActionArea>
-        <CardContent className={styles.Productcardcontent}>
-           <div className={styles.Productcard_div}>{Productname}</div>
+    <Card className={styles.main} sx={{}}>
+      <CardActionArea className={styles.Productcardcontent} >
+        <CardContent >
+           <div className={styles.Productcard_div} >{Productname}</div>
         </CardContent>
       </CardActionArea>
     </Card>
   );
 }
-export default ProductCard;
+export {ProductCard};
