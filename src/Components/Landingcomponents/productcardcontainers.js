62d78d107be288049b38ed4b4a8b200e90d84133
leonkenyaga <jkenyaga@gmail.com>
2023-09-14 23:07:04 +0300

    revised cardcontainer styling

--- src/Components/Landingcomponents/productcardcontainers.js
+++ src/Components/Landingcomponents/productcardcontainers.js
@@ -4,7 +4,7 @@ import styles from "../css/Cardcontainer.module.css"
 
 const Productcardcontainers =()=>{
     return(
-        <Box className={styles.container} sx={{backgroundColor: "secondary.main", height:500}}>
+        <Box className={styles.container} sx={{backgroundColor: "secondary.main"}}>
             <ProductCard Productname="Hooli"/>
         <ProductCard Productname="TradeShare"/>
         <ProductCard Productname="SwingTrader"/>
