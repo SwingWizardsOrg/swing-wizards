556cb9d41f11164d652e26aa5601b86fa90dcf29
leonkenyaga <jkenyaga@gmail.com>
2023-09-14 23:08:44 +0300

    reduced lines of code

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,5 @@
 import React from "react";
 import {Swingappbar} from "../Components/bars/Swingappbar";
-import {ProductCard} from "../Components/Cards/Productcard";
-import styles from  "../Components/css/Home.module.css"
-import { Box } from "@mui/material";
 import { Landingimage } from "../Components/Landingcomponents/Landingimage";
 import { Productcardcontainers } from "../Components/Landingcomponents/productcardcontainers";
 
