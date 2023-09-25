493652a265d7d4a8995e3a01d93948c59de796a7
leonkenyaga <jkenyaga@gmail.com>
2023-09-25 13:29:14 +0300

    rectified merge conflict

--- src/Components/Cards/Productcard.js
+++ src/Components/Cards/Productcard.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import { CardActionArea } from '@mui/material';
 import styles from "../css/Card.module.css";
-import { Outlet, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 const ProductCard =({Productname})=>{
   return (
     <Link to="/signup">
