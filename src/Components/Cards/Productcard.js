d5c37dc417686e192de739a51fc7d40853f9a3ca
leonkenyaga <jkenyaga@gmail.com>
2023-09-25 13:09:15 +0300

    signup link

--- src/Components/Cards/Productcard.js
+++ src/Components/Cards/Productcard.js
@@ -2,16 +2,19 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import { CardActionArea } from '@mui/material';
 import styles from "../css/Card.module.css";
-
+import { Outlet, Link } from "react-router-dom";
 const ProductCard =({Productname})=>{
   return (
+    <Link to="/signup">
     <Card className={styles.main} sx={{}}>
       <CardActionArea>
         <div  className={styles.content} >
            <div className={styles.productname} >{Productname}</div>
         </div>
       </CardActionArea>
+      
     </Card>
+    </Link>
   );
 }
 export {ProductCard};
