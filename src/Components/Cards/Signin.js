01cd147de008824ce18c135dcfbd20f158bd6964
leonkenyaga <jkenyaga@gmail.com>
2023-11-25 19:43:04 +0300

    added signin to sign up switch portal

--- src/Components/Cards/Signin.js
+++ src/Components/Cards/Signin.js
@@ -4,6 +4,7 @@ import styles from "../css/signup.module.css";
 import { Regularinput } from '../Inputs/regularinput';
 import { Passwordinput } from '../Inputs/passwordinput';
 import Button from '@mui/material/Button';
+import { Callout } from './Signup_signin_switch';
 const Signin =()=>{
 const [Inputs, setinputs]=React.useState({
 })
@@ -22,12 +23,19 @@ console.log(Inputs)
   return (
     <Card>
     <div className={styles.main}>
+    <div>
         <div className= {styles.signin}>         
           <Regularinput  Label="Username" Handlechange={handleChange}/>
           <Passwordinput Label= "Password" Handlechange={handleChange}/>
     <Button className={styles.MuiButton} variant="contained" sx={{ml:1.3}} disabled={false} onClick={post}>
         Sign in
       </Button>
+        </div>
+       
+        <div className={styles.Callout}>
+      <Callout Declaration="New to Swing Wizards?" Suggestion="Create an account" Link="/signup"/>
+    </div>
+
         </div>
     </div>
     </Card>
